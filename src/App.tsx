59bc5a56fabaceb635b1babfdbf6f48c37cdc245
Your Name <you@example.com>
2024-12-14 15:17:29 +0530

    created with lot of fun and features

---
+++ src/App.tsx
@@ -0,0 +1,45 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Layout from './components/Layout'
+import { ThemeProvider } from './context/Theme-provider'
+import {WeatherDashboard} from './pages/WeatherDashboard'
+import {CityPage} from './pages/CityPage'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { Toaster } from 'sonner'
+
+
+
+const queryClient = new QueryClient({
+  defaultOptions:{
+    queries:{
+      staleTime: 5 * 60 * 1000,
+      gcTime: 10 * 60 * 1000,
+      retry: false,
+      refetchOnWindowFocus: false,
+    }
+  }
+});
+
+function App() {
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      
+    <BrowserRouter>
+    <ThemeProvider defaultTheme='dark'>
+      <Layout>
+        <Routes>
+          <Route path='/' element={<WeatherDashboard />}/>
+          <Route path='/city/:cityName' element={<CityPage />}/>
+        </Routes>
+      </Layout>
+      <Toaster richColors/>
+    </ThemeProvider>
+    </BrowserRouter>
+    <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
+
+export default App
